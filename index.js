8042236f693c70be78c7988f9bc85693553d689f
ashwinjchhetri <aswinaryal@gmail.com>
2021-05-10 14:47:04 +0545

    updates

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT
+} = require("./config/config");
+
+const postRouter = require("./routes/postRoutes");
 
 const app = express();
+
+const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    })
+    .then(() => {
+      console.log("successfully connected to database");
+    })
+    .catch((err) => {
+      console.log(err);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("<h2>Docker thing is amazing docker-composed </h2>");
 });
+
+app.use("/api/v1/posts", postRouter);
 
 app.listen(port, () => {
   console.log("Listening on port " + port);
